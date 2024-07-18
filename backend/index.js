bb60d5c1a0ceefad24135f2ef534c0d3e8061b45
HaimPik <haimpikman26@gmail.com>
2024-07-18 15:12:46 +0300

    changed4

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const appRouter = require('./routeNotes');
 const Note = require('./note');
 const logger = require('./logger');
 
-const uri = "mongodb+srv://ronhpersonal:123456789David@cluster0.yyfs3xy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" || env.MONGODBURL; //uri from .env doesnt work
+const uri = "mongodb+srv://test_user123:zBKvWonhCKg2rCiX@cluster0.r1sudcq.mongodb.net/presubmission?retryWrites=true&w=majority&appName=Cluster0" || env.MONGODBURL; //uri from .env doesnt work
 
 const app = express();
 app.use(cors());
